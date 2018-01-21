b9189d128d3f1e5e61bb75f649c3f2e75ab45526
charlesartbr <e-mail@charles.art.br>
2018-01-21 09:16:08 -0200

    environment config

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', $_SERVER['SERVER_PORT']=="80"? 'production' : 'development');
 
 /*
  *---------------------------------------------------------------
