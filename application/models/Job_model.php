d251a0a82acc038d11a55bedee657db3983be891
charlesartbr <e-mail@charles.art.br>
2018-01-21 16:30:37 -0200

    models

--- application/models/Job_model.php
+++ application/models/Job_model.php
@@ -4,7 +4,28 @@ class Job_model extends CI_Model
 {
     public function get_all()
     {
-        return $this->db->from('job')->order_by('PublishYear', 'DESC')->order_by('JobId', 'DESC')->get()->result();
+        $jobs = $this->db->from('job')->order_by('PublishYear', 'DESC')->order_by('JobId', 'DESC')->get()->result();
+
+        $this->db->from('job_technology');
+        $this->db->join('technology', 'technology.TechnologyId = job_technology.TechnologyId');
+        $this->db->join('type', 'type.TypeId = technology.TypeId');
+
+        $technogies = $this->db->order_by('type.Priority')->order_by('JobId')->get()->result();
+
+        foreach($jobs as $job)
+        {
+            $job->technologies = array();
+
+            foreach($technogies as $technology)
+            {
+                if($technology->JobId == $job->JobId)
+                {
+                    $job->technologies[] = $technology->TechnologyId;
+                }
+            }
+        }
+
+        return $jobs;
     }
 
     public function get($id = NULL)
@@ -13,7 +34,7 @@ class Job_model extends CI_Model
         {
             return NULL;
         }
-
+        
         $job = $this->db->where('JobId', $id)->get('job')->result();
 
         return $job? $job[0] : NULL;
